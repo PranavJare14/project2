5bf83ff3c19f0e6c5d786100f451091ab323eb30
Pranav Jare <146122534+PranavJare14@users.noreply.github.com>
2025-05-22 23:10:55 +0530

    Delete git&github/project2 directory

--- git&github/project2/READ.md
+++
@@ -1,5 +0,0 @@
-# New project2
-
-new project is added
-
-creted by Pranav Jare.
--- git&github/project2/app.js
+++
@@ -1,6 +0,0 @@
-//add new feature -button
-//add new feature -form
-// adding this on feature branch.
-//new line
-// adding this on feature branch
-
